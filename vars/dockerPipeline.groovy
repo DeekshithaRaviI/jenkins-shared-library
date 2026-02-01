2274cc0e86d36b7bd6cf3d113f42b57a803ee966
Deekshitha <deekshitharavindran7@gmail.com>
2026-02-01 14:51:46 +0530

    Implement Docker pipeline stages in Groovy script

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,3 +1,54 @@
 def call(Map config) {
-    echo "Docker shared pipeline loaded"
+
+    pipeline {
+        agent any
+
+        environment {
+            IMAGE_NAME = config.imageName
+            IMAGE_TAG  = config.imageTag
+            DOCKERHUB  = credentials(config.dockerHubCreds)
+        }
+
+        stages {
+
+            stage('Clone Repository') {
+                steps {
+                    git config.gitRepo
+                }
+            }
+
+            stage('Build Docker Image') {
+                steps {
+                    sh """
+                      docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
+                    """
+                }
+            }
+
+            stage('Run Docker Container') {
+                steps {
+                    sh """
+                      docker run -d -p ${config.containerPort}:${config.containerPort} \
+                      --name demo_container ${IMAGE_NAME}:${IMAGE_TAG} || true
+                    """
+                }
+            }
+
+            stage('Push Image to Docker Hub') {
+                steps {
+                    sh """
+                      echo ${DOCKERHUB_PSW} | docker login -u ${DOCKERHUB_USR} --password-stdin
+                      docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${DOCKERHUB_USR}/${IMAGE_NAME}:${IMAGE_TAG}
+                      docker push ${DOCKERHUB_USR}/${IMAGE_NAME}:${IMAGE_TAG}
+                    """
+                }
+            }
+        }
+
+        post {
+            always {
+                sh "docker rm -f demo_container || true"
+            }
+        }
+    }
 }
